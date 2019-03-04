a75be0d617ef776b661b28504ab1dd7cabf7038d
Ashish Prajapati <ashishprajapati0404@gmail.com>
2019-03-04 17:16:33 +0530

    Features# App controller updated for premsis authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :premises_auhtorization  
+  # before_action :premises_auhtorization  
 
   layout :layout_by_resource
   
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def premises_auhtorization
-  	unless %w(122.168.88.100 125.99.165.4).include?(request.remote_ip)
-  		redirect_to "/404.html" and return
-  	end
-  end
+  # def premises_auhtorization
+  # 	unless %w(122.168.88.100 125.99.165.4).include?(request.remote_ip)
+  # 		redirect_to "/404.html" and return
+  # 	end
+  # end
 end
