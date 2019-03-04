59fc9b79950e1a97362317f98284de6939d639d8
Ashish Prajapati <ashishprajapati0404@gmail.com>
2019-03-04 13:13:06 +0530

    Features# Role fix

--- app/controllers/time_logs_controller.rb
+++ app/controllers/time_logs_controller.rb
@@ -51,7 +51,7 @@ private
  end
 
  def check_role
-  unless current_user.has_role?(:HR) && current_user.has_role?(:admin)
+  unless current_user.has_role?(:HR) || current_user.has_role?(:admin)
     redirect_to root_path
   end 
  end
